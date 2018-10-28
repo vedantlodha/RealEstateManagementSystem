bfcae7eb9bd2fc032236f54c0ea0bf35f58e6460
Vedant <vedant.lodha@hotmail.com>
2018-10-28 17:09:04 +0530

    Revert "GUI(login)"
    This reverts commit 10c3691124526ce728015be9dffb0eccfaf1b347.

--- src/rms/ManagerDB.java
+++ src/rms/ManagerDB.java
@@ -107,18 +107,4 @@ public class ManagerDB {
         return password;
         
     }
-    
-    void updateDues(int customerId,int amount)throws SQLException{
-        int dues=getDues(customerId);
-        dues-=amount;
-        String query="Update table customer set pendingDues="+dues;
-        Statement stmt=conn.createStatement();
-        stmt.executeUpdate(query);
-        //add return value
-    }
-   
-    protected void finalize() throws Throwable {
-       conn.close();
-    }
-    
 }
